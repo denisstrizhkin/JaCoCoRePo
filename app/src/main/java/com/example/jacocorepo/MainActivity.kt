d8bd418296e8cca7da37b435369318d77ae69189
Denis Strizhkin <strdenis02@gmail.com>
2023-04-11 14:08:57 +0300

    Fixed errors

--- app/src/main/java/com/example/jacocorepo/MainActivity.kt
+++ app/src/main/java/com/example/jacocorepo/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.jacocorepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.jacocorepo.databinding.ActivityMainBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var disposeBag = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        disposeBag.add(RxTextView.textChanges(binding.firstEditText)
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .observeOn(Schedulers.io())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .map { it.trim() }
+            .subscribe { text ->
+                Log.i("MyTag", text.toString())
+            }
+        )
+
+        disposeBag.add(RxTextView.textChanges(binding.secondEditText)
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .observeOn(Schedulers.io())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .map { it.trim() }
+            .subscribe { text ->
+                Log.i("MyTag", text.toString())
+            }
+        )
+
+        binding.buttonNext.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposeBag.clear()
     }
 }
